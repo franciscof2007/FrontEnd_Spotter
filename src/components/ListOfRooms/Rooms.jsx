19ed01c9de16c4b34771a7ce46773940b7892cf5
Carolina <vidalcarolinamaria@gmail.com>
2026-08-06 21:23:20 +0100

    Loading

--- src/components/ListOfRooms/Rooms.jsx
+++ src/components/ListOfRooms/Rooms.jsx
@@ -6,6 +6,7 @@ import Error from "../Errors/Error";
 import SemInternet from "../Errors/SemInternet";
 import { useFilters } from "../../hooks/useFilters";
 import SemResultadosFiltros from "../Errors/SemResultadosFiltros";
+import Loading from "../Errors/Loading";
 
 
 function Rooms(){
@@ -50,7 +51,7 @@ function Rooms(){
     if (error || rooms.length===0){
         return(
             <div>
-                <SemResultadosFiltros/>
+                <error/>
             </div>
         );
     }
