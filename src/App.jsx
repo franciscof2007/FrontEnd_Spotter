e3754c253873c03aa778ff7a27f67c1d9b957fce
Carolina <vidalcarolinamaria@gmail.com>
2026-08-11 23:16:14 +0100

    Página Seleção Filtros

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchPage from './components/ListOfRooms/SearchPage';
 import CampusSelection from './components/Campus/CampusSelection';
 import RoomDetails from './components/Details/RoomDetails';
+// 1. Importa o novo componente
+import FiltersAvailability from './components/Filters/FiltersAvailability'; // ajusta o caminho do ficheiro se necessário
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
-        {/* Until the campus page exists, use the search page at the root route so the current work is visible. */}
-        <Route path="/:campus" element={<SearchPage />} />
-       
-        <Route path="/" element={<CampusSelection />} /> 
-        
-        <Route path="/sala/:id" element={<RoomDetails />} /> 
+        {/* Route de teste temporária */}
+        <Route path="/teste" element={<FiltersAvailability />} />
 
+        {/* Rotas existentes da tua aplicação */}
+        <Route path="/:campus" element={<SearchPage />} />
+        <Route path="/" element={<CampusSelection />} /> 
+        <Route path="/sala/:id" element={<RoomDetails />} /> 
       </Routes>
     </BrowserRouter>
   );
