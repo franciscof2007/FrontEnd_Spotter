c04d2218cba45fcc89c7e3c8c8b628ce795d17f3
Francisco Frieza <franciscofrieza@gmail.com>
2026-09-02 00:07:54 +0100

    Permiti que fosse possível ter estado de erro, loading, sucesso ao criar os reports

---
+++ src/hooks/UseCreateReport.js
@@ -0,0 +1,37 @@
+import { useState} from "react";
+import { createReport } from "../Services/ReportsService";
+
+export function useCreateReport(params) {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
+
+    async function submitReport(fenixId, dados) {
+        if (isSubmitting){
+            return;
+        }
+        setIsSubmitting(true);
+        try{
+            await createReport(fenixId, dados);
+            setSuccess(true);
+            
+        }catch(error){
+            setError(error);
+            
+        }finally{
+            setIsSubmitting(false);
+        }
+
+
+
+
+            
+    }
+
+    return{
+        isSubmitting, 
+        error,
+        success,
+        submitReport,
+    };
+}
